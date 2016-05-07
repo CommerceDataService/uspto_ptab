ceb5b8b9089831f1e0931e374edceea1c423c86a
Sasan Bahadaran <sbahadaran@doc.gov>
2016-05-07 10:33:21 -0400

    worked out some bugs and made file splitting fully functional

--- parse_grant_xml.py
+++ parse_grant_xml.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python 3.5
 
 #Author:        Sasan Bahadaran
-#Date:          2/29/16
+#Date:          5/7/16
 #Organization:  Commerce Data Service
-#Description:   This script crawls the files directory and finds the metadata
-#xml file for each set of pdf files.  It then gets the document id from
-#the metadata and finds the associated extracted pdf text and adds it to the
-#structure of the document, then transforms the document to json and saves it
-#to a file.  Lastly, it sends the documents from the json file to Solr for
-#indexing.
+#Description:   This script crawls the files directory and finds each metadata
+#xml file.  It then splits the master file into one xml file per document.
+#Next, it renames core elements and transforms the content to JSON.
+#Lastly, it sends the documents from the json file to Solr for indexing.
 
 import sys, json, xmltodict, os, logging, time, argparse, glob, requests,re
 import dateutil.parser
@@ -27,14 +25,75 @@ def formatDate(x, field):
     dateiso = date.isoformat()+'Z'
     return dateiso
 
-#send document to Solr for indexing
-def sendToSolr(core, json):
-     #add try-catch block
-     jsontext = '{"add":{ "doc":'+json+',"boost":1.0,"overwrite":true, "commitWithin": 1000}}'
-     url = os.path.join(solrURL,"solr",core,"update")
-     headers = {"Content-type" : "application/json"}
-     
-     return requests.post(url, data=jsontext, headers=headers)
+#validate date
+def validDate(s):
+    try:
+        datetime.strptime(s, "%Y")
+        return s
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
+def splitFiles(fname):
+    try:
+        filecontent = []
+        fn = ""
+        dir_path = os.path.join(os.path.dirname(fname),os.path.splitext(fname)[0])
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        with open(os.path.abspath(fname)) as fd:
+            filecontent.append(fd.readline())
+            for line in fd:
+                if line.strip().startswith("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"):
+                    #print("end of doc")
+                    if not os.path.isfile(os.path.join(dir_path)):
+                        with open(os.path.join(dir_path,fn),'w') as outfile:
+                            outfile.writelines(filecontent)
+                    else:
+                        logging.info("--file: "+fn+" already exists")
+                        print("file exists")
+                    del filecontent[:]
+                    filecontent.append(fd.readline())
+                elif line.strip().startswith("<us-patent-grant"):
+                    searchobj = re.findall('file="(.*?)"', line)
+                    fn = searchobj[0]
+                    #print(fn)
+                    filecontent.append(line)
+                else:
+                    filecontent.append(line)
+            if not os.path.isfile(os.path.join(dir_path)):
+                with open(os.path.join(dir_path,fn),'w') as outfile:
+                    outfile.writelines(filecontent)
+                    #print("last file")
+    except IOError as e:
+        logging.error("I/O error({0}): {1}".format(e.errno,e.strerror))
+    except:
+        logging.error("Unexpected error:", sys.exc_info()[0])
+        raise
+
+#this function contains the code for parsing the xml file
+#and writing the results out to a json file
+def parseXML(dir_name):
+    try:
+        for file in dir_name:
+            with open(os.path.abspath(file)) as fd:
+                fn = changeExt(fd,'json')
+                print(fn)
+                doc = xmltodict.parse(fd.read())
+                doc_info = doc['us-patent-grant']['us-bibliographic-data-grant']['publication-reference']['document-id']
+                doc_info['appid'] = doc_info.pop('doc-number')
+                doc_info['doc_date'] = doc_info.pop('date')
+                #textdata - not sure what this should be set to.....
+
+                #transform output to json and save to file with same name
+                with open(fn,'w') as outfile:
+                    json.dump(doc,outfile)
+                    logging.info("-- Processing of XML file complete")
+    except IOError as e:
+        logging.error("I/O error({0}): {1}".format(e.errno,e.strerror))
+    except:
+        logging.error("Unexpected error:", sys.exc_info()[0])
+        raise
 
 def readJSON(fname):
     try:
@@ -67,90 +126,35 @@ def readJSON(fname):
         logging.error("Unexpected error:", sys.exc_info()[0])
         raise
 
-#this function contains the code for parsing the xml file
-#and writing the results out to a json file
-def processXML(fname):
-    try:
-        with open(os.path.abspath(fname)) as fd:
-            fn = changeExt(fname,'json')
-            doc = xmltodict.parse(fd.read())
-            doc_info = doc['us-patent-grant']['us-bibliographic-data-grant']['publication-reference']['document-id']
-            doc_info['appid'] = doc_info.pop('doc-number')
-            doc_info['doc_date'] = doc_info.pop('date')
-            #textdata - not sure what this should be set to.....
-
-            #transform output to json and save to file with same name
-            with open(fn,'w') as outfile:
-                json.dump(doc,outfile)
-                logging.info("-- Processing of XML file complete")
-    except IOError as e:
-        logging.error("I/O error({0}): {1}".format(e.errno,e.strerror))
-    except:
-        logging.error("Unexpected error:", sys.exc_info()[0])
-        raise
-
-#validate date
-def validDate(s):
-    try:
-        datetime.strptime(s, "%Y")
-        return s
-    except ValueError:
-        msg = "Not a valid date: '{0}'.".format(s)
-        raise argparse.ArgumentTypeError(msg)
-
-def splitFiles(fname):
-    try:
-        filecontent = []
-        fn = ""
-        dir_path = os.path.join(os.path.dirname(fname),os.path.splitext(fname)[0])
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
-        with open(os.path.abspath(fname)) as fd:
-            filecontent.append(fd.readline())
-            for line in fd:
-                if line.strip().startswith("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"):
-                    #print("end of doc")
-                    if not os.path.isfile(os.path.join(dir_path)):
-                        with open(os.path.join(dir_path,fn),'w') as outfile:
-                            outfile.writelines(filecontent)
-                    else:
-                        logging.info("--file: "+fn+" already exists")
-                        print("file exists")
-                    del filecontent[:]
-                    #print(line)
-                    filecontent.append(fd.readline())
-                elif line.strip().startswith("<us-patent-grant"):
-                    searchobj = re.findall('file="(.*?)"', line)
-                    fn = searchobj[0]
-                    #print(fn)
-                    filecontent.append(line)
-                else:
-                    filecontent.append(line)
-            if not os.path.isfile(os.path.join(dir_path)):
-                with open(os.path.join(dir_path,fn),'w') as outfile:
-                    outfile.writelines(filecontent)
-                    #print("last file")
-    except IOError as e:
-        logging.error("I/O error({0}): {1}".format(e.errno,e.strerror))
-    except:
-        logging.error("Unexpected error:", sys.exc_info()[0])
-        raise
-
-def processFile(filename):
-    fn = changeExt(filename,'json')
-    if os.path.isfile(os.path.abspath(fn)):
+#send document to Solr for indexing
+def sendToSolr(core, json):
+     #add try-catch block
+     jsontext = '{"add":{ "doc":'+json+',"boost":1.0,"overwrite":true, "commitWithin": 1000}}'
+     url = os.path.join(solrURL,"solr",core,"update")
+     headers = {"Content-type" : "application/json"}
+     
+def processFile(fname):
+    logging.info("-- Processing file: "+fname)
+    dir_path = os.path.join(os.path.dirname(fname),os.path.splitext(fname)[0])
+    if (args.skipsplit):
+        logging.info("-- Skipping File Split process")
+        logging.info("-- Starting XML Parse process")
+        #parseXML(dir_path)
         if (args.skipsolr):
-            logging.info("-- XML file: "+filename+" already processed.  Skipping Solr process.")
+            logging.info("-- Skipping Solr process.")
         else:
-            logging.info("-- XML file: "+filename+" already processed.  Sending to Solr for indexing.")
-            #readJSON(fn)
+            logging.info("-- Starting Solr process")
+            #readJSON(dir_path)
     else:
-        logging.info("-- Starting splitting of XML file: "+filename)
-        splitFiles(filename)
-        logging.info("-- Starting processing of XML file: "+filename)
-        #processXML(filename)
-        logging.info("-- Starting processing of JSON file: "+fn)
-        #readJSON(fn)
+        logging.info("-- Starting File Split process")
+        splitFiles(fname)
+        logging.info("-- Starting XML Parse process")
+        #parseXML(dir_path)
+        if (args.skipsolr):
+            logging.info("-- Skipping Solr process") 
+        else:
+             logging.info("-- Starting Solr process")
+             #readJSON(dir_path)
 
 if __name__ == '__main__':
     scriptpath = os.path.dirname(os.path.abspath(__file__))
@@ -158,7 +162,7 @@ if __name__ == '__main__':
 
     #logging configuration
     logging.basicConfig(
-                        filename='logs/parsegrantxml-log-'+time.strftime('%Y%m%d'),
+                        filename='logs/parsegrant-log-'+time.strftime('%Y%m%d'),
                         level=logging.INFO,
                         format='%(asctime)s - %(name)s - %(levelname)s -%(message)s',
                         datefmt='%Y%m%d %H:%M:%S'
@@ -178,14 +182,19 @@ if __name__ == '__main__':
                         "--skipsolr",
                         required=False,
                         help="Pass this flag to skip Solr processing",
-                        action='store_false'
+                        action='store_true'
                        )
-
+    parser.add_argument(
+                        "-p",
+                        "--skipsplit",
+                        required=False,
+                        help="Pass this flag to skip File Split process",
+                        action='store_true'
+                       )
     args = parser.parse_args()
-    logging.info("--SCRIPT ARGUMENTS--------------")
     if args.dates:
-        logging.info("Date arguments passed for processing: "+", ".join(args.dates))
-    logging.info("Solr Processing set to: "+str(args.skipsolr))
+        logging.info("Date arguments set to: "+",".join(args.dates))
+    #logging.info("File Split set to: "+str(args.skipsplit))
     logging.info("-- [JOB START]  ----------------")
 
     if args.dates:
