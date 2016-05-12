9b7814a8056ad6ada2efa3de0c4f0b49a3651200
Sasan Bahadaran <sbahadaran@doc.gov>
2016-05-12 15:09:30 -0400

    modified iglob file search to exclude alt.xml files

--- parse_grant_xml.py
+++ parse_grant_xml.py
@@ -61,7 +61,7 @@ def outputFile(fname, input):
 def combineFiles(fname):
     try:
         filecontent = []
-        fn = os.path.splitext(fname)[0]+"_altered.xml"
+        fn = os.path.splitext(fname)[0]+"_alt.xml"
         if not os.path.isfile(fn):
             with open(os.path.abspath(fname)) as fd:
                 filecontent.append(fd.readline())
@@ -81,10 +81,10 @@ def combineFiles(fname):
         else:
             logging.info("-- File: "+fn+" already exists")
     except IOError as e:
-        logging.error("I/O error({0}): {1}".format(e.errno,e.strerror))
+        logging.error("-- I/O error({0}): {1}".format(e.errno,e.strerror))
         raise
     except:
-        logging.error("Unexpected error:", sys.exc_info()[0])
+        logging.error("-- Unexpected error:", sys.exc_info()[0])
         raise
 
 #this function contains the code for parsing the xml file
@@ -101,19 +101,19 @@ def parseXML(fname):
                     line['doc_date'] = line.pop('date')
                     #textdata field needs to be set also, but that is yet to be determined
 
-                #transform output to json and save to file with same name
+            #transform output to json and save to file with same name
             outputFile(fn,json.dumps(doc))
             logging.info("-- Processing of XML file complete")
         else:
-            logging.info("--File: "+fn+" already exists.")
+            logging.info("-- File: "+fn+" already exists.")
 
     except KeyError as e:
-        logging.error("File: "+fn+" Key Error: {0} ".format(e))
+        logging.error("-- File: "+fn+" Key Error: {0} ".format(e))
         pass
     except IOError as e:
-        logging.error("I/O error({0}): {1}".format(e.errno,e.strerror))
+        logging.error("-- I/O error({0}): {1}".format(e.errno,e.strerror))
     except:
-        logging.error("Unexpected error:", sys.exc_info()[0])
+        logging.error("-- Unexpected error:", sys.exc_info()[0])
         raise
 
 def readJSON(fname):
@@ -126,39 +126,44 @@ def readJSON(fname):
                 with open(os.path.join(os.path.dirname(fname),'solrcomplete.txt'),'a+') as logfile:
                     logfile.seek(0)
                     if docid+"\n" in logfile:
-                       logging.info("-- file: "+docid+"  already processed by Solr")
+                       logging.info("-- File: "+docid+"  already processed by Solr")
                        continue
                     else:
                        logging.info("-- Sending file: "+docid+" to Solr")
-                       response = sendToSolr('grants', jsontext)
-                       r = response.json()
-                       status = r["responseHeader"]["status"]
-                       if status == 0:
-                           logfile.write(docid+"\n")
-                           logging.info("-- Solr update for file: "+docid+" complete")
-                       else:
-                           logging.info("-- Solr error for doc: "+docid+" error: "+', '.join("{!s}={!r}".format(k,v) for (k,v) in rdict.items()))
+
+
+                       logfile.write(docid+"\n")
+
+
+
+                       #response = sendToSolr('grants', jsontext)
+                       #r = response.json()
+                       #status = r["responseHeader"]["status"]
+                       #if status == 0:
+                       #    logfile.write(docid+"\n")
+                       #    logging.info("-- Solr update for file: "+docid+" complete")
+                       #else:
+                       #    logging.info("-- Solr error for doc: "+docid+" error: "+', '.join("{!s}={!r}".format(k,v) for (k,v) in rdict.items()))
     except IOError as e:
-        logging.error("I/O error({0}): {1}".format(e.errno,e.strerror))
+        logging.error("-- I/O error({0}): {1}".format(e.errno,e.strerror))
     except:
-        logging.error("Unexpected error:", sys.exc_info()[0])
+        logging.error("-- Unexpected error:", sys.exc_info()[0])
         raise
 
 #send document to Solr for indexing
 def sendToSolr(core, json):
-     #add try-catch block
      try:
          jsontext = '{"add":{ "doc":'+json+',"boost":1.0,"overwrite":true, "commitWithin": 1000}}'
          url = os.path.join(solrURL,"solr",core,"update")
          headers = {"Content-type" : "application/json"}
      except:
-         logging.error("Unexpected error: ", sys.exc_info()[0])
+         logging.error("-- Unexpected error: ", sys.exc_info()[0])
 
 def processFile(fname):
     logging.info("-- Processing file: "+fname)
-    altfn = os.path.splitext(fname)[0]+"_altered.xml"
+    altfn = os.path.splitext(fname)[0]+"_alt.xml"
     fn = changeExt(altfn,'json')
-    if (args.skipsplit):
+    if (args.skipcombine):
         logging.info("-- Skipping File Split process")
         logging.info("-- Starting XML Parse process")
         parseXML(altfn)
@@ -215,10 +220,10 @@ if __name__ == '__main__':
                         action='store_true'
                        )
     parser.add_argument(
-                        "-p",
-                        "--skipsplit",
+                        "-c",
+                        "--skipcombine",
                         required=False,
-                        help="Pass this flag to skip File Split process",
+                        help="Pass this flag to skip File Combine process",
                         action='store_true'
                        )
     args = parser.parse_args()
@@ -228,28 +233,24 @@ if __name__ == '__main__':
     elif args.type == "p":
         filetype.append("PUBS")
         filetype.append("application")
-    logging.info("File type set to: "+args.type)
+    logging.info("--------ARGUMENTS------------")
+    logging.info("File type set to: "+filetype[0])
     if args.dates:
         logging.info("Date arguments set to: "+",".join(args.dates))
-    logging.info("Skip File Split set to: "+str(args.skipsplit))
+    logging.info("Skip File Combine set to: "+str(args.skipcombine))
     logging.info("Skip Solr set to: "+str(args.skipsolr))
     logging.info("-- [JOB START]  ----------------")
-
-    #if args.type == "g":
-    #    filetype.append("GRANTS")
-    #    filetype.append("grant")
-    #elif args.type == "p":
-    ##   filetype.append("PUBS")
-    #    filetype.append("application")
 
     if args.dates:
         for date in args.dates:
             #crawl through each main directory and find the metadata xml file
             for filename in glob.iglob(os.path.join(scriptpath,'files',filetype[0],date,'*.xml'),recursive=True):
-                processFile(filename)
+                if not filename.endswith("_alt.xml"):
+                    processFile(filename)
     else:
         #crawl through each main directory and find the metadata xml file
         for filename in glob.iglob(os.path.join(scriptpath,'files',filetype[0],'*/*.xml')):
-            processFile(filename)
+            if not filename.endswith("_alt.xml"):
+                processFile(filename)
 
     logging.info("-- [JOB END] ----------------")
